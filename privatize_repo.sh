b4cef95398843c70754958ee0e6c6ca3d6c77d18
Madhav Seth <85406105+Madhav-Sheth@users.noreply.github.com>
2022-06-16 21:06:08 +0530

    increase variables even more!

--- privatize_repo.sh
+++ privatize_repo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /tmp/ci
 source "${CONFIG}"
-curl -X PATCH -H "Accept: application/vnd.github.nebula-preview+json" -H "Authorization: token $GIT_TOKEN" https://api.github.com/repos/bruhminusprotestingorg/java-ce-build -d '{"visibility":"private"}'
+curl -X PATCH -H "Accept: application/vnd.github.nebula-preview+json" -H "Authorization: token $GIT_TOKEN" https://api.github.com/repos/"${GIT_ORG_NAME}"/"${NAME_OF_THIS_REPOSITORY}" -d '{"visibility":"private"}'
