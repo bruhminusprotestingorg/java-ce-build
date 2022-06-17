60dcc904bf520a6c2faf447f0e852724a7724d60
Madhav Seth <85406105+Madhav-Sheth@users.noreply.github.com>
2022-06-17 19:36:29 +0530

    git

--- additional.sh
+++ additional.sh
@@ -5,4 +5,6 @@ curl -ksSLo ~/.gitcookies "${GIT_COOKIES_RAW_GIST}"
 chmod 0600 ~/.gitcookies
 git config --global http.cookiefile ~/.gitcookies
 sudo touch /etc/mtab; sudo chmod 777 /etc/mtab
+git config --global http.sslverify false
+export GIT_SSL_NO_VERIFY=1
 echo Success!
