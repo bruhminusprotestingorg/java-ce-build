f134468e0537f2414d8c48194856ff1594d44394
bruhminusprotesting <96990463+bruhminusprotesting@users.noreply.github.com>
2022-06-21 21:42:06 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,4 +13,4 @@ export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
 TARGET_USES_PREBUILT_KERNEL=false
 lunch "${ROM_LUNCH_NAME}"_"${DEVICE_CODENAME}"-"${BUILD_TYPE}"
-mka bacon -j18"
+mka bacon -j18" −−preserve−status; RESULT="$?"; echo $RESULT; if [[ "$RESULT" == "124" ]]; then exit 0; else exit $RETURN; fi
