bdee8c950b6256c8d63e4f837e48079273fb2447
bruhminusprotesting <96990463+bruhminusprotesting@users.noreply.github.com>
2022-06-22 01:40:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,9 @@ cd /tmp/rom
 ls && pwd
 ccache -M 20G # It took only 6.4GB for mido
 ccache -o compression=true # Will save times and data to download and upload ccache, also negligible performance issue
-timeout 100m bash -c "source build/envsetup.sh
+
+SECONDS=$(< /tmp/ci/seconds.txt)
+timeout "$((6660 - $SECONDS))"s bash -c "source build/envsetup.sh
 export TARGET_KERNEL_CLANG_VERSION=proton
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
