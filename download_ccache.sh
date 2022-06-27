e3a7e8b0154239aaef670f3f1c49d049a19fca54
bruhminusprotesting <96990463+bruhminusprotesting@users.noreply.github.com>
2022-06-27 15:44:19 +0600

    crdroid-lava: build with ccache run 3

--- download_ccache.sh
+++ download_ccache.sh
@@ -5,4 +5,4 @@ cd /tmp # Where to download cccahe
 while [[ ! -f ccache.tar.gz ]]; do time aria2c "${CCACHE_INDEX_DOWNLOAD_URL}" -x16 -s50 ; done  # Using aria2c for speed haha, ccache will be at /tmp/ccache.tar.gz since my file name is ccache.tar.gz
 time tar xf ccache.tar.gz  # Extract ccache so ci can use it
 rm -rf ccache.tar.gz # Remove unnecessary downloaded file, it will speed up the upload ccache process
-CCACHE_RUN=2
+CCACHE_RUN=3
