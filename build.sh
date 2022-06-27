790b1db544c5f52d229319b4bea39f6c05881112
bruhminusprotesting <96990463+bruhminusprotesting@users.noreply.github.com>
2022-06-27 09:30:41 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,8 +4,6 @@ source "${CONFIG}"
 
 cd /tmp/rom
 ls && pwd
-ccache -M 20G # It took only 6.4GB for mido
-ccache -o compression=true # Will save times and data to download and upload ccache, also negligible performance issue
 
 SECONDS=$(< /tmp/ci/seconds.txt)
 timeout "$((6660 - $SECONDS))"s bash -c "source build/envsetup.sh
@@ -13,6 +11,8 @@ export TARGET_KERNEL_CLANG_VERSION=proton
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
+export ccache -M 20G
+export ccache -o compression=true
 TARGET_USES_PREBUILT_KERNEL=false
 lunch "${ROM_LUNCH_NAME}"_"${DEVICE_CODENAME}"-"${BUILD_TYPE}"
 mka bacon -j18" −−preserve−status; RESULT="$?"; echo $RESULT; if [[ "$RESULT" == "124" ]]; then exit 0; else exit $RESULT; fi
