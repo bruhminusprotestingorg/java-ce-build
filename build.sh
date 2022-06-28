77553a1c3711ad0ecd514cfae981c28d08aa935d
bruhminusprotesting <96990463+bruhminusprotesting@users.noreply.github.com>
2022-06-28 12:39:54 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -11,8 +11,8 @@ export TARGET_KERNEL_CLANG_VERSION=proton
 export CCACHE_DIR=/tmp/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-export ccache -M 20G
-export ccache -o compression=true
+ccache -M 20G
+ccache -o compression=true
 TARGET_USES_PREBUILT_KERNEL=false
 lunch "${ROM_LUNCH_NAME}"_"${DEVICE_CODENAME}"-"${BUILD_TYPE}"
 mka bacon -j18" −−preserve−status; RESULT="$?"; echo $RESULT; if [[ "$RESULT" == "124" ]]; then exit 0; else exit $RESULT; fi
