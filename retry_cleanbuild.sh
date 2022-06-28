0e42c3be3f66c51ca3c1ba518308142d1c232aaa
bruhminusprotesting <96990463+bruhminusprotesting@users.noreply.github.com>
2022-06-28 15:46:02 +0530

    Update retry_cleanbuild.sh

--- retry_cleanbuild.sh
+++ retry_cleanbuild.sh
@@ -7,12 +7,14 @@ retry_build() {
     if [[ "${GIT_ORG_NAME}" == "none" ]]; then GIT_ORG_NAME="${GIT_USER_NAME}" ; fi
     git clone --depth=1 https://"${GIT_USER_NAME}":"${GIT_TOKEN}"@github.com/"${GIT_ORG_NAME}"/"${NAME_OF_THIS_REPOSITORY}" -b "${CURRENT_BRANCH_OF_THIS_REPO}" "${NAME_OF_THIS_REPOSITORY}"
     cd "${NAME_OF_THIS_REPOSITORY}"
-    echo $RANDOM$RANDOM$RANDOM > dummy.txt
+    export $(tail -1 rty.txt)
+    RETRY=$(expr $RETRY + 1)
+    echo 'RETRY='$RETRY > rty.txt
     sed -i '$ d' download_ccache.sh
     echo 'CCACHE_RUN=0' >> download_ccache.sh
-    git add dummy.txt download_ccache.sh
-    git commit -m "$ROM_NAME-$DEVICE_CODENAME: Rom Build Failed, Retrying Clean Build!"
+    git add download_ccache.sh rty.txt
+    git commit -m "$ROM_NAME-$DEVICE_CODENAME: Rom Build Failed, Retrying Clean Build Retry $RETRY"
     git push -f https://"${GIT_USER_NAME}":"${GIT_TOKEN}"@github.com/"${GIT_ORG_NAME}"/"${NAME_OF_THIS_REPOSITORY}"
     exit 0
 }
-if [[ "$(< rty.txt)" -ge "3" ]]; then echo "Build Failed!" && exit 1; else retry_build ; fi
+if [[ "$(< rty.txt)" -ge "4" ]]; then echo "Build Failed!" && exit 1; else retry_build ; fi
