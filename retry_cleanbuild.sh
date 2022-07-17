07670b58e22ba20af49230de34f9ec7978b402b1
bruhminusprotesting <96990463+bruhminusprotesting@users.noreply.github.com>
2022-07-17 11:38:33 +0530

    Update retry_cleanbuild.sh

--- retry_cleanbuild.sh
+++ retry_cleanbuild.sh
@@ -3,6 +3,7 @@ cd /tmp/ci
 source "${CONFIG}"
 
 retry_build() {
+    bash /tmp/ci/publisize_repo.sh
     cd /tmp/
     if [[ "${GIT_ORG_NAME}" == "none" ]]; then GIT_ORG_NAME="${GIT_USER_NAME}" ; fi
     git clone --depth=1 https://"${GIT_USER_NAME}":"${GIT_TOKEN}"@github.com/"${GIT_ORG_NAME}"/"${NAME_OF_THIS_REPOSITORY}" -b "${CURRENT_BRANCH_OF_THIS_REPO}" "${NAME_OF_THIS_REPOSITORY}"
